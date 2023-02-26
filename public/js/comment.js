61bb1f7048e8e91a2ed6d81f33e84007549846de
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-02-25 18:28:53 -0800

    edit js

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,5 +3,43 @@ const { response } = require("express");
 const newCommentHandler = async (event) => {
     event.preventDefault();
 
-    const 
-}
+    const id = document.getElementById('new-comment').getAttribute('data-id');
+    const commentContent = document.getElementById('new-comment').value.trim();
+
+    if (id && commentContent) {
+        const response = await fetch(`/api/comments/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({ commentContent }),
+            header: { "Content-Type": "application/json"},
+        });
+    };
+
+    if(response.ok) {
+        document.location.replace('/');
+    } else {
+        console.log(err);
+        alert('Failed to add comment.');
+    }
+};
+
+const editCommentHandler = async (event) => {
+    event.preventDefault();
+
+    const id = document.getElementById('edit-comment').getAttribute('data-id');
+    const commentContent = document.getElementById('edit-comment').value.trim();
+
+    if (id && commentContent) {
+        const response = await fetch(`/api/comments/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({ commentContent }),
+            header: { "Content-Type": "application/json"},
+        });
+    };
+
+    if(response.ok) {
+        document.location.replace('/');
+    } else {
+        console.log(err);
+        alert('Failed to add comment.');
+    }
+};
